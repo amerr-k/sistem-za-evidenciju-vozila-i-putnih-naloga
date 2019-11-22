13c7dffe1d471791e4fd79f2a989b5c02b455d74
Amer Kovac <amer.kovac.pmf@gmail.com>
2019-11-22 02:09:03 +0100

    modified model names

---
+++ sistem-za-evidenciju-vozila-i-putnih-naloga.Data/Models/CarBrand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistem_za_evidenciju_vozila_i_putnih_naloga.Data.Models
+{
+    public class CarBrand
+    {
+        public int CarBrandId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ sistem-za-evidenciju-vozila-i-putnih-naloga.Data/Models/CarModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistem_za_evidenciju_vozila_i_putnih_naloga.Data.Models
+{
+    public class CarModel
+    {
+        public int CarModelId { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey(nameof(CarModel))]
+        public int CarBrandId { get; set; }
+        public CarBrand CarBrand { get; set; }
+    }
+}
