8240762318410036af69b4cb7f03c1e4219aba3c
Amer Kovac <amer.kovac.pmf@gmail.com>
2019-11-23 13:03:32 +0100

    created new migration

--- sistem-za-evidenciju-vozila-i-putnih-naloga.Data/Migrations/20191122201354_updatedDriver.Designer.cs
+++ sistem-za-evidenciju-vozila-i-putnih-naloga.Data/Migrations/20191122201354_updatedDriver.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sistem_za_evidenciju_vozila_i_putnih_naloga.Data;
 
 namespace sistem_za_evidenciju_vozila_i_putnih_naloga.Data.Migrations
 {
     [DbContext(typeof(SEVPNContext))]
-    partial class SEVPNContextModelSnapshot : ModelSnapshot
+    [Migration("20191122201354_updatedDriver")]
+    partial class updatedDriver
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
