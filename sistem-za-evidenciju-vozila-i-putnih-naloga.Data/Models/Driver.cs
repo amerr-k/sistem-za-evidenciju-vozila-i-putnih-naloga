5a5d634024c921db06fbfd318eb18bce289779b4
Amer Kovac <amer.kovac.pmf@gmail.com>
2019-11-23 13:05:57 +0100

    created models and enum that travel warrant operations uses

---
+++ sistem-za-evidenciju-vozila-i-putnih-naloga.Data/Models/Driver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sistem_za_evidenciju_vozila_i_putnih_naloga.Data.Models
+{
+    public class Driver
+    {
+        public int DriverId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+    }
+}
