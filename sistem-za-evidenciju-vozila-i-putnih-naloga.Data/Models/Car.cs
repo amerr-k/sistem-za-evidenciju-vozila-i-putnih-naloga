5a6a1dbafc96a4d7c435c2cbb330c736fd3b04d1
Amer Kovac <amer.kovac.pmf@gmail.com>
2019-11-27 15:46:03 +0100

    modified car controller, added float numbers

--- sistem-za-evidenciju-vozila-i-putnih-naloga.Data/Models/Car.cs
+++ sistem-za-evidenciju-vozila-i-putnih-naloga.Data/Models/Car.cs
@@ -16,10 +16,10 @@ namespace sistem_za_evidenciju_vozila_i_putnih_naloga.Data.Models
 
         public string ChassisNumber { get; set; }
         public string EngineNumber { get; set; }
-        public double EnginPowerKS { get; set; }
-        public double EnginPowerKW { get; set; }
+        public float EnginPowerKS { get; set; }
+        public float EnginPowerKW { get; set; }
         public Fuel Fuel{ get; set; }
-        public int ProductionYear{ get; set; }
+        public int? ProductionYear{ get; set; }
 
     }
 }
