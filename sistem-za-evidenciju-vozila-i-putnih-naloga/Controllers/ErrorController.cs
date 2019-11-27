c87771ad9bdfb87f685cda99560c38b6b6c20a34
Amer Kovac <amer.kovac.pmf@gmail.com>
2019-11-27 15:42:50 +0100

    added error controller and travel warrant exception handler

---
+++ sistem-za-evidenciju-vozila-i-putnih-naloga/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sistem_za_evidenciju_vozila_i_putnih_naloga.Controllers
+{
+    [Route("Error/{statusCode}")]
+    public class ErrorController : Controller
+    {
+        public IActionResult HttpExceptionHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewData["ErrorMessage"] = "Resurs nije pronađen";
+                    break;
+            }
+            return View();
+        }
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
